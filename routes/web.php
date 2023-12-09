7301f1e7872fcad407ef18f280dd3f6e6289720b
Elgrande-Eyub <ayoubfcb2002@gmail.com>
2023-12-09 21:01:48 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,6 @@
 use App\Http\Controllers\invioce;
 use Illuminate\Support\Facades\Route;
 use Barryvdh\DomPDF\Facade\Pdf;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
    return redirect('admin');
@@ -20,4 +10,3 @@ Route::get('/', function () {
 
 Route::get('/invioce/{id}', [invioce::class,'downloadPDF'])->name('downPDF');
 Route::get('/facture/{id}', [invioce::class,'facturePDF'])->name('facturePDF');
-
